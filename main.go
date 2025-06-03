ec8455ef42eee96c604571c706bace8cd6c9d060
Hoon Jeong <you@example.com>
2025-06-02 22:39:02 -0400

    more handlers

--- main.go
+++ main.go
@@ -6,12 +6,31 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox!"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("SnippetView!"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed!"))
+		return
+	}
+	w.Write([]byte("SnippetCreate!"))
+}
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)

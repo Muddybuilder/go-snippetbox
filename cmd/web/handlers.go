9c9798646607a558d6e841ca1249b9e511907a44
Hoon Jeong <you@example.com>
2025-06-05 22:19:11 -0400

    separation of handler and logs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,46 +3,52 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
-
 	files := []string{
 		"./ui/html/base.tmpl",
-		"./ui/html/pages/home.tmpl",
 		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Interneal Server Error", http.StatusInternalServerError)
+		// Because the home handler function is now a method against application
+		// it can access its fields, including the error logger. We'll write the log
+		// message to this instead of the standard logger.
+		app.errorLog.Print(err.Error())
+		app.serverError(w, err)
 		return
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		// Also update the code here to use the error logger from the application
+		// struct.
+		app.errorLog.Print(err.Error())
+		app.serverError(w, err)
 	}
 }
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+// Change the signature of the snippetCreate handler so it is defined as a method
+// against *application.
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
